f77cac5feecbdfd7f64f2eada6b7335b15b7e81b
Tibor <tibor736@hotmail.com>
2023-09-08 16:42:35 +0300

    Added About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from './components/About';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
@@ -7,7 +8,8 @@ const App = () => {
 		<>
 			<Navbar />
 			<Hero />
-      <Skills />
+			<Skills />
+			<About />
 		</>
 	);
 };
